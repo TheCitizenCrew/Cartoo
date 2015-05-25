288517af1f8e1c03518468388344a03e547a6567
Cyrille Giquello <cyrille@giquello.fr>
2015-05-25 13:25:14 +0200

    app kernel settings

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends HttpKernel {
 	 */
 	protected $routeMiddleware = [
 		'auth' => 'App\Http\Middleware\Authenticate',
-		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+		//'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
 	];
 
