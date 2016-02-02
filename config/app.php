af15297abc889091967cad51c45437898691c9aa
Cyrille Giquello <cyrille@giquello.fr>
2016-02-02 01:19:34 +0100

    add eloquent-oauth

--- config/app.php
+++ config/app.php
@@ -1,19 +1,18 @@
 <?php
-
-return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Environment
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
-    |
-    */
-
-    'env' => env('APP_ENV', 'production'),
+return [ 
+	
+	/*
+	 * |--------------------------------------------------------------------------
+	 * | Application Environment
+	 * |--------------------------------------------------------------------------
+	 * |
+	 * | This value determines the "environment" your application is currently
+	 * | running in. This may determine how you prefer to configure various
+	 * | services your application utilizes. Set this in your ".env" file.
+	 * |
+	 */
+	
+	'env' => env( 'APP_ENV', 'production' ),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +25,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env( 'APP_DEBUG', false ),
 
     /*
     |--------------------------------------------------------------------------
@@ -91,9 +90,9 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
-
-    'cipher' => 'AES-256-CBC',
+    'key' => env( 'APP_KEY' ),
+	
+	'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
@@ -108,7 +107,7 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => env( 'APP_LOG', 'single' ),
 
     /*
     |--------------------------------------------------------------------------
@@ -121,42 +120,48 @@ return [
     |
     */
 
-    'providers' => [
+    'providers' => [ 
 
-        /*
-         * Laravel Framework Service Providers...
-         */
-        Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Cookie\CookieServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Hashing\HashServiceProvider::class,
-        Illuminate\Mail\MailServiceProvider::class,
-        Illuminate\Pagination\PaginationServiceProvider::class,
-        Illuminate\Pipeline\PipelineServiceProvider::class,
-        Illuminate\Queue\QueueServiceProvider::class,
-        Illuminate\Redis\RedisServiceProvider::class,
-        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-
+		/*
+		 * Laravel Framework Service Providers...
+		 */
+		Illuminate\Auth\AuthServiceProvider::class,
+		Illuminate\Broadcasting\BroadcastServiceProvider::class,
+		Illuminate\Bus\BusServiceProvider::class,
+		Illuminate\Cache\CacheServiceProvider::class,
+		Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+		Illuminate\Cookie\CookieServiceProvider::class,
+		Illuminate\Database\DatabaseServiceProvider::class,
+		Illuminate\Encryption\EncryptionServiceProvider::class,
+		Illuminate\Filesystem\FilesystemServiceProvider::class,
+		Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+		Illuminate\Hashing\HashServiceProvider::class,
+		Illuminate\Mail\MailServiceProvider::class,
+		Illuminate\Pagination\PaginationServiceProvider::class,
+		Illuminate\Pipeline\PipelineServiceProvider::class,
+		Illuminate\Queue\QueueServiceProvider::class,
+		Illuminate\Redis\RedisServiceProvider::class,
+		Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+		Illuminate\Session\SessionServiceProvider::class,
+		Illuminate\Translation\TranslationServiceProvider::class,
+		Illuminate\Validation\ValidationServiceProvider::class,
+		Illuminate\View\ViewServiceProvider::class,
+		
+		/*
+		 * Eloquent OAuth service provider and assets for Laravel 5
+		 * https://github.com/adamwathan/eloquent-oauth-l5
+		 */
+		AdamWathan\EloquentOAuthL5\EloquentOAuthServiceProvider::class,
+    		
         /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-    ],
+		App\Providers\AuthServiceProvider::class,
+		App\Providers\EventServiceProvider::class,
+		App\Providers\RouteServiceProvider::class 
+	]
+	,
 
     /*
     |--------------------------------------------------------------------------
@@ -169,39 +174,41 @@ return [
     |
     */
 
-    'aliases' => [
-
-        'App'       => Illuminate\Support\Facades\App::class,
-        'Artisan'   => Illuminate\Support\Facades\Artisan::class,
-        'Auth'      => Illuminate\Support\Facades\Auth::class,
-        'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Cache'     => Illuminate\Support\Facades\Cache::class,
-        'Config'    => Illuminate\Support\Facades\Config::class,
-        'Cookie'    => Illuminate\Support\Facades\Cookie::class,
-        'Crypt'     => Illuminate\Support\Facades\Crypt::class,
-        'DB'        => Illuminate\Support\Facades\DB::class,
-        'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
-        'Event'     => Illuminate\Support\Facades\Event::class,
-        'File'      => Illuminate\Support\Facades\File::class,
-        'Gate'      => Illuminate\Support\Facades\Gate::class,
-        'Hash'      => Illuminate\Support\Facades\Hash::class,
-        'Lang'      => Illuminate\Support\Facades\Lang::class,
-        'Log'       => Illuminate\Support\Facades\Log::class,
-        'Mail'      => Illuminate\Support\Facades\Mail::class,
-        'Password'  => Illuminate\Support\Facades\Password::class,
-        'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Redirect'  => Illuminate\Support\Facades\Redirect::class,
-        'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Request'   => Illuminate\Support\Facades\Request::class,
-        'Response'  => Illuminate\Support\Facades\Response::class,
-        'Route'     => Illuminate\Support\Facades\Route::class,
-        'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Session'   => Illuminate\Support\Facades\Session::class,
-        'Storage'   => Illuminate\Support\Facades\Storage::class,
-        'URL'       => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View'      => Illuminate\Support\Facades\View::class,
-
-    ],
-
-];
+    'aliases' => [ 
+		
+		'App' => Illuminate\Support\Facades\App::class,
+		'Artisan' => Illuminate\Support\Facades\Artisan::class,
+		'Auth' => Illuminate\Support\Facades\Auth::class,
+		'Blade' => Illuminate\Support\Facades\Blade::class,
+		'Cache' => Illuminate\Support\Facades\Cache::class,
+		'Config' => Illuminate\Support\Facades\Config::class,
+		'Cookie' => Illuminate\Support\Facades\Cookie::class,
+		'Crypt' => Illuminate\Support\Facades\Crypt::class,
+		'DB' => Illuminate\Support\Facades\DB::class,
+		'Eloquent' => Illuminate\Database\Eloquent\Model::class,
+		'Event' => Illuminate\Support\Facades\Event::class,
+		'File' => Illuminate\Support\Facades\File::class,
+		'Gate' => Illuminate\Support\Facades\Gate::class,
+		'Hash' => Illuminate\Support\Facades\Hash::class,
+		'Lang' => Illuminate\Support\Facades\Lang::class,
+		'Log' => Illuminate\Support\Facades\Log::class,
+		'Mail' => Illuminate\Support\Facades\Mail::class,
+		'Password' => Illuminate\Support\Facades\Password::class,
+		'Queue' => Illuminate\Support\Facades\Queue::class,
+		'Redirect' => Illuminate\Support\Facades\Redirect::class,
+		'Redis' => Illuminate\Support\Facades\Redis::class,
+		'Request' => Illuminate\Support\Facades\Request::class,
+		'Response' => Illuminate\Support\Facades\Response::class,
+		'Route' => Illuminate\Support\Facades\Route::class,
+		'Schema' => Illuminate\Support\Facades\Schema::class,
+		'Session' => Illuminate\Support\Facades\Session::class,
+		'Storage' => Illuminate\Support\Facades\Storage::class,
+		'URL' => Illuminate\Support\Facades\URL::class,
+		'Validator' => Illuminate\Support\Facades\Validator::class,
+		'View' => Illuminate\Support\Facades\View::class,
+    		
+    	'SocialAuth' => AdamWathan\EloquentOAuth\Facades\OAuth::class
+	]
+	 
+]
+;
